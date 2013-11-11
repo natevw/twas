795d5111bd0c11c51a859df000e2cb67e2c5111f
Nathan Vander Wilt <nate@calftrail.com>
2013-11-11 14:57:23 -0800

    (new) also encode url, this has confused me before

--- shows/files.js
+++ shows/files.js
@@ -4,9 +4,9 @@ function (doc, req) {
     provides("html", function () {
         var db_url = '/' + req.info.db_name,
             app_url = fs.to_html("{{{db_url}}}/{{{id}}}", {db_url:db_url, id:ddoc._id});
-        var template = "<li><a href='{{db_url}}/{{id}}/{{name}}'>./{{ name }}</a></li>",
+        var template = "<li><a href='{{db_url}}/{{id}}/{{name}}'>./{{ encName }}</a></li>",
             files = (doc && doc._attachments) ? Object.keys(doc._attachments).map(function (name) {
-                return fs.to_html(template, {db_url:db_url, id:doc._id, name:name});
+                return fs.to_html(template, {db_url:db_url, id:doc._id, name:name, encName:encodeURIComponent(name).replace(/%20/g,'+')});
             }).join('') : "<li class='empty'>No files.</li>";
         var may_upload = (doc) ? 'yes' : '';
         return fs.to_html(ddoc.templates.files, {
